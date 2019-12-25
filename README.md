56dfafae808c32d9bf9898bb0ee801d6aa29f2b8
Minoru Tomobe <w-ockham@users.noreply.github.com>
2019-12-26 01:56:57 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,134 @@
+# LoRaBot
+
+[Arduino LoRa](https://github.com/sandeepmistry/arduino-LoRa/blob/master/README.md)を使ったLoRaのChatbotです。
+
+##ハードウェア
+
+ * [Semtech SX1276/77/78/79](http://www.semtech.com/apps/product.php?pn=SX1276)を使ったボードで動作します。:
+   * [BSFrance LoRa32u4](https://bsfrance.fr/lora-long-range/1311-BSFrance-LoRa32u4-1KM-Long-Range-Board-Based-Atmega32u4-433MHz-LoRA-RA02-Module.html)
+
+### Semtech SX1276/77/78/79 
+
+| Semtech SX1276/77/78/79 | Arduino |
+| :---------------------: | :------:|
+| VCC | 3.3V |
+| GND | GND |
+| SCK | SCK |
+| MISO | MISO |
+| MOSI | MOSI |
+| NSS | 8 |
+| NRESET | 4 |
+| DIO0 | 7 |
+
+
+`NSS`,`NRESET,`DIO0`は`LoRa.setPins(csPin, resetPin, irqPin)`. で接続することが出来ます。ボードによってピン接続を変更して下さい。
+
+## インストール方法
+
+### Arduino IDEにLoRa32u4ボードを追加
+1. ファイル→環境設定で、追加のボードマネージャのURLに以下を追加
+   https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
+2. ツール→ボードでAdaFruit Feather32u4を選択
+3. ツール→書込装置でArduinoISPを選択
+
+### Arduino LoRaライブラリの追加
+1. ツール→ライブラリマネージャで、LoRa by Sandeep Mistryを追加
+
+### コンパイルと書き込み
+1. LoRaBot.inoを開く
+1．スケッチ→検証・コンパイルでコンパイル
+2. スケッチ→マイコンボードに書き込むでArduinoへ書き込み
+
+### スマートフォンとの接続
+1. スマホに[USBシリアル端末](https://play.google.com/store/apps/details?id=jp.sugnakys.usbserialconsole&hl=ja)をインストール
+2. [OTGケーブル](https://www.amazon.co.jp/dp/B012V56C8K)とmicroUSBケーブルを使ってLoRa32u4をスマホに接続
+3. シリアルポートの設定でボーレート9600 データビット8 パリティnone ストップビット1 フロー制御offを選択
+4. 接続の設定で送信フォームの表示、改行コードCR+LFを選択
+
+## 使い方
+2台のLoRa32u4に書き込みます。サーバ側をモバイルバッテリ、クライアント側をスマホに接続します。
+通常Botモードで起動するので、クライアント側は以下のコマンドでノーマルモードにしてください。
+```sh
+set mode norm
+```
+USB端末ソフトからメッセージを送ると438.2MHzでLoRa変調でメッセージを送ります。
+（保証認定を受け免許されるまでは必ずダミーロード等を使って実験して下さい）
+デフォルトではSF=10、BW=62.5kHz TxPower = 20dBmの設定になっています。
+メッセージの送信が完了すると以下のように表示されます。
+```sh
+ Send: <送信したメッセージ>
+メッセージを受信すると以下の
+```sh
+ Recv(<RSSI値>,<SNR値>,<周波数エラー値>) : <受信したメッセージ> 
+```
+Bot側では送られて来たメッセージに受信時のRSSI(信号強度)、SNR、周波数エラー値を加えてオウム返しします。
+
+## コマンド
+### 周波数の設定
+運用周波数をHzで指定します。指定できる範囲は438MHz-439MHz(全電波形式)です。
+デフォルト値は438.2MHzです。
+```sh
+ set freq 432000000
+```
+### 送信出力の設定
+送信出力をdBmで指定します。2dBm - 20dBmの範囲です。
+デフォルト値は20dBmです。
+```sh
+ set pwr 20
+```
+### 拡散率(SF)の指定
+拡散率(Spreading Factor)を指定します。6 - 12の範囲です。
+小さい値ほど高速に送信できますがSNRでは不利になります。
+デフォルト値は10です。
+```sh
+ set sf 10
+```
+### 帯域幅(BW)の指定
+チャープスペクトラムの帯域幅を指定します。0-8の範囲です。
+デフォルト値は4(31.25kH)です。
+```sh
+ set bw 4
+ ```
+| BW値 | 帯域幅 |
+|:-----|:-------|
+|0 | 7.8kHz |
+|1 | 10.4kHz |
+|2 | 15.6kHz |
+|3 | 20.8kHz |
+|4 | 31.25kHz |
+|5 | 41.7kHz |
+|6 | 62.5kHz |
+|7 | 125kHz |
+|8 | 250kHz |
+
+### 動作モードの指定
+端末の動作モードを指定します。
+デフォルト値はBOTです。
+スプリアス測定等で連続信号が必要な場合はCONTモードを使って下さい。
+```sh
+set mode NORM
+```
+|モード| 動作 |
+|:----:|:----:|
+|NORM | 通常モード|
+|BOT  | 送られたメッセージにRSSI/SNR等を付けて返す|
+|CONT | 0fillされた255byteパケットを連続送信します|
+
+### 自局コールサインの指定
+自局のコールサインを指定します。パケット先頭には必ず自局コールサインが入ります。
+```sh
+set call コールサイン
+```
+
+###リモート端末の設定
+LoRaでは送信側・受信側のSF/BW等が合っていないと交信することができません。
+自局と共に相手局側のパラメータを同時に変更するコマンドです。
+```sh
+rset コマンド パラメータ
+```
+コマンド・パラメータはsetと同じです。
+リモート側端末は新たに変更されたパラメータでテスト送信を行いますので
+ローカル端末で正しく受信できるか確認してください。
+
+## ライセンス
+ Apache License 2.0で配布します。
---
+++ src/LoRaBot.ino
@@ -0,0 +1,268 @@
+/*
+ * LoRa Automatic Responder
+ * 
+ * by JL1NIE 2019/12/24
+ */
+ #include <SPI.h>
+ #include <LoRa.h>
+
+const int csPin = 8;         // LoRa radio chip select.
+const int resetPin = 4;      // LoRa radio reset
+const int irqPin = 7;        // LoRa radio IRQ
+
+const long freq_low = 438e6;
+const long freq_high = 439e6;
+long frequency = 438.2E6;
+int power = 20;
+int spreadingFactor = 10;
+long bandWidthTable[] = { 7.8e3, 10.4e3, 15.6e3, 20.8e3, 31.25e3, 41.7e3, 62.5e3, 125e3, 250e3};
+int bandWidth = 6;
+int prlen = 8;
+bool crc = false;
+bool codingRate = 5;
+int syncWord = 0x12;
+enum {
+  MODE_NORM,
+  MODE_BOT,
+  MODE_CONT
+};
+int mode = MODE_BOT;
+bool remote_set = false;
+
+String mycall,hiscall;
+String line_buffer;
+String remote_command;
+
+void Initialize_LoRaParam() {
+  frequency = 438.2e6;
+  if (!LoRa.begin(frequency)) {              // initialize ratio at 438.2 MHz
+      Serial.println("LoRa init failed. Check your connections.");
+      while (true);                       // if failed, do nothing
+  }
+  bandWidth = 4;
+  LoRa.setSignalBandwidth(bandWidthTable[bandWidth]);     // BW = 31.25kHz
+  spreadingFactor = 10;
+  LoRa.setSpreadingFactor(spreadingFactor);  // SF = 10
+  power = 20;
+  LoRa.setTxPower(power);
+  codingRate = 5;
+  LoRa.setCodingRate4(codingRate);
+  prlen = 8;
+  LoRa.setPreambleLength(prlen);
+  syncWord = 0x12;
+  LoRa.setSyncWord(syncWord);
+  mycall = "JL1NIE";
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initializing LoRa module");
+  LoRa.setPins(csPin, resetPin, irqPin);     // set CS, reset, IRQ pin
+  Initialize_LoRaParam();
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+  Serial.println("LoRa init succeeded.");
+  String f = String(frequency);
+  Serial.println("Listening freq = " + f);
+  line_buffer = "";
+}
+
+int split(String buf, char delim, String *dst) {
+  int index = 0;
+  int arySize = (sizeof(buf)/sizeof((buf)[0]));
+  int len = buf.length();
+
+  for (int i = 0; i < len; i++) {
+    char tmp = buf.charAt(i);
+    if (tmp == delim) {
+      index++;
+      if ( index > (arySize -1)) return -1;
+    }
+    else dst[index] += tmp;
+  }
+  return (index + 1);
+}
+
+void do_command(String buffer) {
+  String cmds[3] = {"\0"};
+
+  buffer.toUpperCase();
+  buffer.trim();
+  
+  int index = split(buffer, ' ', cmds);
+  if ( index == 1 ) {
+    String sf = String(spreadingFactor);
+    Serial.println("SF = " + sf);
+    String bw = String(bandWidthTable[bandWidth]);
+    Serial.println("BW = " + bw);
+    String pw = String(power);
+    Serial.println("TX Power = " + pw);
+    return;
+  } else if ( index != 3 ) {
+    if ( cmds[1] == "INIT" ) {
+      LoRa.end();
+      Initialize_LoRaParam();
+      return;
+    } 
+    Serial.println("Command error: " + buffer);
+    return;
+  }
+  
+  if ( cmds[1] == "FREQ" ) {
+    int freq = cmds[2].toInt();
+    if ( freq >= freq_low && freq <= freq_high ) {
+      frequency = freq;
+      LoRa.setFrequency(frequency);
+      Serial.println("Set frequency = " + cmds[2]);
+    } else {
+      Serial.println("frequency error : " + cmds[2]);
+    }
+    return;
+  }
+  
+  if ( cmds[1] == "PWR" ) {
+    int p = cmds[2].toInt();
+    if ( p >= 2 && p <= 20 ) {
+      power = p;
+      LoRa.setTxPower(power);
+      Serial.println("Set TX power = " + cmds[2]);
+    } else {
+      Serial.println("TX power error = " + cmds[2]);
+    }
+    return;
+  }
+  
+  if ( cmds[1] == "SF" ) {
+    int sf = cmds[2].toInt();
+    if ( sf >= 6 && sf <= 12 ) {
+      spreadingFactor = sf;
+      LoRa.setSpreadingFactor(spreadingFactor);
+      Serial.println("Set Spreading Factor = " + cmds[2]);
+    } else {
+      Serial.println("Spreading Factor error = " + cmds[2]);
+    }
+    return;
+  }
+  
+  if ( cmds[1] == "BW" ) {
+    int bw = cmds[2].toInt();
+    if ( bw >= 0 && bw <= 8 ) {
+        bandWidth = bw;
+        String bws = String(bandWidthTable[bw]);
+        LoRa.setSignalBandwidth(bandWidthTable[bw]);
+        Serial.println("Set BW = " + bws + "Hz");
+        return;
+    }
+    Serial.println("BW error = " + cmds[2]);
+    return;
+  }
+     
+  if ( cmds[1] == "CALL") {
+    mycall = cmds[2];
+    Serial.println("Set my callsign = " + cmds[2]);
+    return;
+  }
+  
+  if ( cmds[1] == "MODE") {
+    if ( cmds[2] == "NORM" ) {
+      mode = MODE_NORM;
+      Serial.println("Normal Mode");
+      return;
+    }
+    if ( cmds[2] == "BOT" ) {
+      mode = MODE_BOT;
+      Serial.println("Robot Mode");
+      return;
+    }
+    if ( cmds[2] == "CONT" ) {
+      mode = MODE_CONT;
+      Serial.println("Continuous Signal Output mode.");
+      return;
+    }
+  }
+}
+
+void loop() {
+  char c;
+  if ( Serial.available() ) {
+       c = Serial.read();
+       if ( c == '\r' ) {
+          Serial.read();// Skip LF
+          if ( line_buffer.indexOf("rset") == 0){
+              LoRa.beginPacket();
+              LoRa.print(line_buffer);
+              LoRa.endPacket();
+              LoRa.receive();
+              do_command(line_buffer);
+          } else if ( line_buffer.indexOf("set") == 0) {
+              do_command(line_buffer);
+          } else {
+            if ( mode == MODE_CONT ) {
+              Serial.println("Sending 255 bytes of Zero...");
+              for (int i = 0; i < 128; i++ ) {
+                LoRa.beginPacket();
+                for (int j = 0; j < 255; j++ ) 
+                  LoRa.write(0);
+                LoRa.endPacket();
+                if ( Serial.available() ) 
+                    break;
+                Serial.println("*");
+              };
+              Serial.println("done.");
+              LoRa.receive();
+            } else {
+              String msg = "DE " + mycall + " " +line_buffer;
+              LoRa.beginPacket();
+              LoRa.print(msg);
+              LoRa.endPacket();
+              Serial.println("Send: " + msg);
+              LoRa.receive();
+            }
+          }
+          line_buffer = "";
+       } else {
+        line_buffer += c;
+       }
+  }
+  if (remote_set) {
+   remote_set =false;
+   do_command(remote_command);
+   for ( int i = 0; i < 3 ; i++) {
+    delay(2000);
+    LoRa.beginPacket();
+    LoRa.print("DE " + mycall + " VVV VVV VVV ");
+    LoRa.endPacket();
+   }
+   LoRa.beginPacket();
+   LoRa.print("DE " + mycall + " " + "(done.)");
+   LoRa.endPacket();
+   LoRa.receive();
+  }
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  String rssi = String(LoRa.packetRssi());
+  String snr = String(LoRa.packetSnr());
+  String ferr = String(LoRa.packetFrequencyError());
+  String rprt = "RSSI=" + rssi + " SNR=" + snr + " Ferr=" + ferr;
+
+  if ( incoming.indexOf("rset") >= 0) {
+    remote_set = true;
+    remote_command = incoming;
+  };
+  Serial.println("Recv("+rssi+","+snr+","+ferr+"):" + incoming);
+  if ( mode == MODE_BOT ) {
+    LoRa.beginPacket();
+    LoRa.print("DE " + mycall + " UR "+ rprt + " : " + incoming);
+    LoRa.endPacket();
+    LoRa.receive();
+  }     
+}
